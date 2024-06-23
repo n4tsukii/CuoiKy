d1d516826c609a780c74a39d0057b55feef1a16c
LyGiaKhanh <yeumails456@gmail.com>
2024-06-24 02:21:10 +0700

    commit

--- profile/profile.js
+++ profile/profile.js
@@ -1,39 +1,24 @@
-const thread = document.querySelector(".threads");
-const replies = document.querySelector(".replies");
-const logo = document.querySelector("#threadlogo");
-const header = document.querySelector(".header");
-const qrcode = document.querySelector("#qrcode-img");
-const hiddenReplies = document.querySelector(".replies-post");
-const main = document.querySelectorAll(".thread-main");
-const repliesBtn = document.querySelectorAll(".replies-post");
-const aLinks = document.querySelector("#a-links");
+function openTab(event, tabName) {
+  var i, tabcontent, tablinks;
+  
+  // Hide all tab contents
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  
+  // Remove the 'active' class from all tab links
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  
+  // Show the current tab and add 'active' class to the clicked tab
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active";
+}
 
-replies.addEventListener("click", () => {
-  repliesBtn.forEach((x) => {
-    x.classList.remove("hide");
-  });
-
-  main.forEach((x) => {
-    x.classList.add("hide");
-  });
-
-  hiddenReplies.classList.add("show");
-});
-
-thread.addEventListener("click", () => {
-  repliesBtn.forEach((x) => {
-    x.classList.add("hide");
-  });
-
-  main.forEach((x) => {
-    x.classList.remove("hide");
-  });
-});
-
-logo.addEventListener("click", () => {
-  document.body.classList.toggle("bgchange");
-  thread.classList.toggle("bgchange");
-  replies.classList.toggle("bgchange");
-  aLinks.classList.toggle("textchange");
-  document.body.style.transition = "1s";
+// Set default active tab
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("threads").style.display = "block";
 });
