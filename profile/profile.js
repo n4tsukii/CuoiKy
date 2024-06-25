328bbc863d9eb321d489d62e545a202fea7f36a2
LyGiaKhanh <yeumails456@gmail.com>
2024-06-26 03:34:32 +0700

    COMMIT UPDATE

--- profile/profile.js
+++ profile/profile.js
@@ -1,24 +1,32 @@
+/* profile.js */
 function openTab(event, tabName) {
   var i, tabcontent, tablinks;
-  
-  // Hide all tab contents
   tabcontent = document.getElementsByClassName("tab-content");
   for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
   }
-  
-  // Remove the 'active' class from all tab links
   tablinks = document.getElementsByClassName("tab-link");
   for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  
-  // Show the current tab and add 'active' class to the clicked tab
   document.getElementById(tabName).style.display = "block";
   event.currentTarget.className += " active";
 }
 
-// Set default active tab
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("threads").style.display = "block";
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementsByClassName('tab-link')[0].click();
 });
+
+function toggleLike(element) {
+  element.classList.toggle('fa-regular');
+  element.classList.toggle('fa-solid');
+  element.style.color = element.style.color === 'red' ? '' : 'red';
+}
+
+function toggleComment(element) {
+  // Implement comment functionality here
+}
+
+function toggleRetweet(element) {
+  // Implement retweet functionality here
+}
